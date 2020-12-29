3948187b17957b7c6df3641c29affd08b026d036
3A732045 <jessezzxxcc@gmail.com>
2020-12-29 19:36:44 +0800

    在TaskController 新增destroy  方法，刪除任務（有授權）

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -30,5 +30,13 @@ class TaskController extends Controller
         ]);
         return redirect('/tasks');
     }
+    public function destroy(Request $request, Task $task)
+    {
+        $this->authorize('destroy', $task);
+
+        $task->delete();
+
+        return redirect('/tasks');
+    }
 }
 
